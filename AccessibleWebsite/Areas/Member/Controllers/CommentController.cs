f40c183e2d738b525b4024dc7c64f69d34c32901
LcnKadir <144541247+LcnKadir@users.noreply.github.com>
2024-03-12 21:40:05 +0300

    MemberArea(CommentController/EditComment.cshtml)
    CommentController düzenlendi ve EditComment.cshtml sayfası tekrardan ele alınarak, buglar fixlendi. Sidebar’a ilgili linkler verildi.

--- AccessibleWebsite/Areas/Member/Controllers/CommentController.cs
+++ AccessibleWebsite/Areas/Member/Controllers/CommentController.cs
@@ -5,63 +5,53 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace AccessibleWebsite.Areas.Member.Controllers
 {
-    [Area("Member")]
-    public class CommentController : Controller
-    {
-        private readonly ICommentService _commentService;
-        private readonly UserManager<AppUser> _userService;
-        private readonly IBlogService _blogService;
+	[Area("Member")]
+	public class CommentController : Controller
+	{
+		private readonly ICommentService _commentService;
+		private readonly UserManager<AppUser> _userService;
+		private readonly IBlogService _blogService;
 
-        public CommentController(ICommentService commentService, UserManager<AppUser> userService, IBlogService blogService)
-        {
-            _commentService = commentService;
-            _userService = userService;
-            _blogService = blogService;
-        }
+		public CommentController(ICommentService commentService, UserManager<AppUser> userService, IBlogService blogService)
+		{
+			_commentService = commentService;
+			_userService = userService;
+			_blogService = blogService;
+		}
 
-        [HttpGet]
-        public async Task<IActionResult> Index()
-        {
-            var user = await _userService.FindByNameAsync(User.Identity.Name);
+		[HttpGet]
+		public async Task<IActionResult> Index()
+		{
+			var user = await _userService.FindByNameAsync(User.Identity.Name);
+			var comment = await _commentService.GetCommentWithBlogList(user.Id);
+			return View(comment);
+		}
 
-            var comment = await _commentService.GetCommentWithBlogList(user.Id);
-            return View(comment);
-        }
-
-        [HttpGet]
-        public async Task<IActionResult> EditComment(int id)
-        {
-            var value = await _userService.FindByNameAsync(User.Identity.Name);
-            ViewBag.userId = value.Id;
-            ViewBag.BlogId = await _commentService.GetByIdAsync(id);
-
-            var user = await _commentService.GetByIdAsync(id);
-            return View(user);
-        }
+		[HttpGet]
+		public async Task<IActionResult> EditComment(int id)
+		{
+			var user = await _commentService.GetByIdAsync(id);
+			return View(user);
+		}
 
 
-        [HttpPost]
-        public async Task<IActionResult> EditComment(Comment comment, int id)
-        {
-            var value = await _userService.FindByNameAsync(User.Identity.Name);
-            ViewBag.userId = value.Id;
-
-            ViewBag.BlogId = await _commentService.GetByIdAsync(id);
-
-            await _commentService.UpdateAsync(comment);
-            return RedirectToAction(nameof(Index));
-        }
+		[HttpPost]
+		public async Task<IActionResult> EditComment(Comment comment, int id)
+		{
+			await _commentService.UpdateAsync(comment);
+			return RedirectToAction(nameof(Index));
+		}
 
 
 
-        public async Task<IActionResult> DeleteComment(int id)
-        {
+		public async Task<IActionResult> DeleteComment(int id)
+		{
 
-            var user = await _commentService.GetByIdAsync(id);
-            await _commentService.RemoveAsync(user);
-            return RedirectToAction(nameof(Index));
-        }
+			var user = await _commentService.GetByIdAsync(id);
+			await _commentService.RemoveAsync(user);
+			return RedirectToAction(nameof(Index));
+		}
 
 
-    }
+	}
 }
