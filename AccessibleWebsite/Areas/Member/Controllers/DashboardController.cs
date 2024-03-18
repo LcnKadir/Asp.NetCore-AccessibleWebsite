8f4373d196e33fd7b44aa635f574c11571ed655e
LcnKadir <144541247+LcnKadir@users.noreply.github.com>
2024-03-18 14:41:18 +0300

    [Member Area] – Navbar revize edildi/ Ana sayfa düzenlendi ve buglar fixlendi.

--- AccessibleWebsite/Areas/Member/Controllers/DashboardController.cs
+++ AccessibleWebsite/Areas/Member/Controllers/DashboardController.cs
@@ -8,7 +8,6 @@ namespace AccessibleWebsite.Areas.Member.Controllers
     [Area("Member")]
     public class DashboardController : Controller
     {
-
         public async Task<IActionResult> Index()
         {
             return View();
--- AccessibleWebsite/ViewComponents/Blog/_LastBlogs.cs
+++ AccessibleWebsite/ViewComponents/Blog/_LastBlogs.cs
@@ -6,16 +6,20 @@ namespace AccessibleWebsite.ViewComponents.Blog
     public class _LastBlogs : ViewComponent
     {
         private readonly IBlogService _blogService;
+        private readonly ICategoryService _categoryService;
 
-        public _LastBlogs(IBlogService blogService)
+        public _LastBlogs(IBlogService blogService, ICategoryService categoryService)
         {
             _blogService = blogService;
+            _categoryService = categoryService;
+
         }
 
         [HttpGet]
         public async Task<IViewComponentResult> InvokeAsync(int id)
         {
             var values = await _blogService.GetLastBlogAsync(id);
+            await _categoryService.GetAllAsync();
             return View(values);
 
         }
