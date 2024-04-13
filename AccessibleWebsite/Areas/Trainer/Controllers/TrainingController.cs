c8e1549843504d84dcc22c2bc80e73c4c3cdb95d
LcnKadir <144541247+LcnKadir@users.noreply.github.com>
2024-04-13 15:41:59 +0300

    Trainer Area [TrainingController]

---
+++ AccessibleWebsite/Areas/Trainer/Controllers/TrainingController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AccessibleWebsite.Areas.Trainer.Controllers
+{
+    public class TrainingController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
