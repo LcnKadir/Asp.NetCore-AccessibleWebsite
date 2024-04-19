a9148c33a92b04f27eb39bb283c4c07fb243529a
LcnKadir <144541247+LcnKadir@users.noreply.github.com>
2024-04-19 10:06:25 +0300

    Trainer Area [Dashboard/Index.chtml]

--- AccessibleWebsite/obj/Debug/net7.0/AccessibleWebsite.AssemblyInfo.cs
+++ AccessibleWebsite/obj/Debug/net7.0/AccessibleWebsite.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AccessibleWebsite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4067fc6ebb88b2299e1ce7965f7f4b6edddeb7ff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94f9b3d58c98fa6f92c5982cd668485b373635c5")]
 [assembly: System.Reflection.AssemblyProductAttribute("AccessibleWebsite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AccessibleWebsite")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
