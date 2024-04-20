dfe7fb6bc2f273f316a9ccb49bffe9d96d5fec4e
LcnKadir <144541247+LcnKadir@users.noreply.github.com>
2024-04-20 16:52:41 +0300

    Admin Area [ListUsersController/ListUsers.cshtml]

---
+++ AccessibleWebsite/Areas/Admin/Controllers/UsersController.cs
@@ -0,0 +1,22 @@
+﻿using CoreLayer.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AccessibleWebsite.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class UsersController : Controller
+    {
+        private readonly IAppUserService _appUserService;
+
+        public UsersController(IAppUserService appUserService)
+        {
+            _appUserService = appUserService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ListUsers()
+        {
+            return View();
+        }
+    }
+}
