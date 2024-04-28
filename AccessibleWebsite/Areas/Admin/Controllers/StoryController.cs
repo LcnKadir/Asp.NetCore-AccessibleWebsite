c791d549a35f307d1d4ad079edb70e33adf324da
LcnKadir <144541247+LcnKadir@users.noreply.github.com>
2024-04-28 11:49:42 +0300

    Admina Area [StroyController/Index] üyelerin paylaştıkları hikayeler ile birlikte listelenmesi gerçekleşti

--- AccessibleWebsite/Areas/Admin/Controllers/StoryController.cs
+++ AccessibleWebsite/Areas/Admin/Controllers/StoryController.cs
@@ -1,13 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CoreLayer.Services;
+using Microsoft.AspNetCore.Mvc;
 
 namespace AccessibleWebsite.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class StoryController : Controller
     {
-        public IActionResult Index()
+        private readonly IStoryService _storyService;
+
+        public StoryController(IStoryService storyService)
         {
-            return View();
+            _storyService = storyService;
+        }
+
+        [HttpGet]
+        public async  Task<IActionResult> Index()
+        {
+            var users = await _storyService.GetAllStoryAsync();
+            return View(users);
         }
     }
 }
--- AccessibleWebsite/obj/Debug/net7.0/AccessibleWebsite.AssemblyInfo.cs
+++ AccessibleWebsite/obj/Debug/net7.0/AccessibleWebsite.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AccessibleWebsite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0bdc247c6c85315c99010f39b428dc883bbbd7e8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2a261087bf0d7b640af654cb584e009632aa531")]
 [assembly: System.Reflection.AssemblyProductAttribute("AccessibleWebsite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AccessibleWebsite")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
