454358ec45c7fa013745c67d51be7392f1f0f83a
LcnKadir <144541247+LcnKadir@users.noreply.github.com>
2024-05-06 14:57:37 +0300

    AccessibleWebsite Area / TrainersController-AccessibleTrainersIndex.cshtml

---
+++ AccessibleWebsite/Areas/Accessible/Controllers/TrainersController.cs
@@ -0,0 +1,49 @@
+﻿using CoreLayer.Models;
+using CoreLayer.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AccessibleWebsite.Areas.Accessible.Controllers
+{
+    [Area("Accessible")]
+    public class TrainersController : Controller
+    {
+        private readonly IAppUserService _userService;
+        private readonly ITrainingService _trainingService;
+        private readonly UserManager<AppUser> _userManager;
+
+        public TrainersController(IAppUserService userService, ITrainingService trainingService, UserManager<AppUser> userManager)
+        {
+            _userService = userService;
+            _trainingService = trainingService;
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> AccessibleTrainersIndex() 
+        {
+            var appUsers = await _userService.GetTrainers();
+            return View(appUsers);
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> AccessibleGetTraining(int id)
+        {
+            await _trainingService.GetAllTrainingAsync();
+            var trainer = await _userManager.Users.Where(x => x.TrainerId == id).FirstOrDefaultAsync();
+            return View(trainer);
+        }
+
+        [Authorize]
+        [HttpPost]
+        public async Task<IActionResult> AccessibleAddTraining(Training training)
+        {
+            training.CreateDate = Convert.ToDateTime(DateTime.Now.ToShortDateString());
+            await _trainingService.AddTrainingAsync(training);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
--- AccessibleWebsite/obj/Debug/net7.0/AccessibleWebsite.AssemblyInfo.cs
+++ AccessibleWebsite/obj/Debug/net7.0/AccessibleWebsite.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AccessibleWebsite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53ad8ff69a5f680304af98c67b6fab678ed978d4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5541f1e38978d451ad2ff38e434b6bc9eb73e0b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("AccessibleWebsite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AccessibleWebsite")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
