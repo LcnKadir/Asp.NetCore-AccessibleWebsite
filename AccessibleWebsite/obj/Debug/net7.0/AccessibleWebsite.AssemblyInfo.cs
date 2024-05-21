98399f076c4a43930380cb17c01b36bd7cfcfe36
LcnKadir <144541247+LcnKadir@users.noreply.github.com>
2024-05-21 15:05:25 +0300

    Erişilebilir site ve Blog düzenleme sayfası Refactoring edildi

--- AccessibleWebsite/obj/Debug/net7.0/AccessibleWebsite.AssemblyInfo.cs
+++ AccessibleWebsite/obj/Debug/net7.0/AccessibleWebsite.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AccessibleWebsite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27a15c8366f1fdc3baddb0a1e180f6fb2bb5ec12")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3e3529843f08d27f34056ccafcc6ec31266c8e2")]
 [assembly: System.Reflection.AssemblyProductAttribute("AccessibleWebsite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AccessibleWebsite")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
